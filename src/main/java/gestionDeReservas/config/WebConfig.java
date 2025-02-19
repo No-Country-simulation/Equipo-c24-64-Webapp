0e9cc073f49fd913a41c01991907e04ea0b08323
federicoPardo999 <federicopardo944@gmail.com>
2025-02-19 16:25:26 -0300

    comennt webconfig

--- src/main/java/gestionDeReservas/config/WebConfig.java
+++ src/main/java/gestionDeReservas/config/WebConfig.java
@@ -1,27 +1,27 @@
-package gestionDeReservas.config;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-                .allowedOriginPatterns("*") 
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);;
-			}
-		};
-	}
-}
+//package gestionDeReservas.config;
+//
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowCredentials(true);
+//			}
+//		};
+//	}
+//}
